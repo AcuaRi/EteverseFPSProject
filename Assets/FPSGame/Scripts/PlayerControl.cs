ee063d9c78e7b5dc382b0cae73c68bb327cf7847
AcuaRi <acuarium2307@gmail.com>
2024-05-13 15:34:42 +0900

    플레이어 애니메이션 전환 로직 추가

--- Assets/FPSGame/Scripts/PlayerControl.cs
+++ Assets/FPSGame/Scripts/PlayerControl.cs
@@ -6,6 +6,8 @@ namespace FPSGame
         {
             // 이동 속도.
             [SerializeField] private float moveSpeed = 5f;
+            // Animator 컴포넌트 변수.
+            [SerializeField] private Animator refAnimator;
             
             // 트랜스폼 컴포넌트 참조 변수.
             private Transform refTransform;
@@ -14,13 +16,25 @@ namespace FPSGame
             {
                 // 트랜스폼 참조 저장.
                 refTransform = transform;
+                refAnimator = GetComponentInChildren<Animator>();
             }
     
             private void Update()
             {
                 float horizontal = Input.GetAxis("Horizontal");
                 float vertical = Input.GetAxis("Vertical");
-
+                
+                
+                
+                if (horizontal ==0 && vertical ==0)
+                {
+                    refAnimator.SetInteger("State", 0);
+                }
+                else
+                {
+                    refAnimator.SetInteger("State", 1);
+                }
+                
                 refTransform.position += moveSpeed * Time.deltaTime * new Vector3(horizontal, 0, vertical).normalized;
                 
             }
